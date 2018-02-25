2634304e7f7bf68c8ac6ab692198c69c33f2ffb3
aabidkar <aabidkar@yahoo.com>
2018-02-25 10:32:47 +0530

    Fixes

--- finsys/src/test/java/ejagruti/finsys/modules/createCompany.java
+++ finsys/src/test/java/ejagruti/finsys/modules/createCompany.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import ejagruti.finsys.pageobjects.baseclass;
--- finsys/src/test/java/ejagruti/finsys/modules/runTest.java
+++ finsys/src/test/java/ejagruti/finsys/modules/runTest.java
@@ -1,29 +1,19 @@
 package ejagruti.finsys.modules;
 
-import org.junit.runner.RunWith; 
-import cucumber.api.CucumberOptions;
-
-
+import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"},
-features = {"src/test/resources/FeatureCollections"},
-tags={"@SmokeTest"}
-)
-public class runTest extends AbstractTestNGCucumberTests{
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, features = {
+		"src/test/resources/FeatureCollections" }, tags = { "@SmokeTest" })
+public class runTest {
 }
 /*
-@RunWith(Cucumber.class) 
-@CucumberOptions.Options(format = {"pretty", "html:target/html"},
-features = {"FeatureCollections"},
-tags={"@SmokeTest"}
-	) 
-public class runTest {
-
-}
-*/
+ * @RunWith(Cucumber.class)
+ * 
+ * @CucumberOptions.Options(format = {"pretty", "html:target/html"}, features =
+ * {"FeatureCollections"}, tags={"@SmokeTest"} ) public class runTest {
+ * 
+ * }
+ */
